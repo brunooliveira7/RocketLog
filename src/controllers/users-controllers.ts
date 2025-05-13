60e471bd1472b7ffbab991fb33263fa0d49c0541
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-05-13 15:01:12 -0300

    validating user data - retrieved from the body - with zod

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from "express";
+import { z } from "zod";
 
 class UsersController {
   create(request: Request, response: Response) {
+    //validar o body com o zod - se não passar na validação, vai retornar um erro
+    const bodySchema = z.object({
+      name: z.string().trim().min(3),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    //recuperar o body da requisição
+    const { name, email, password } = bodySchema.parse(request.body);
+
     return response.json({ message: "create user" });
   }
 }
