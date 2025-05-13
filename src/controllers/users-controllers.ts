1c4c4f3b6c7c3b8ff41aefc477ff31096812e6c2
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-05-13 15:13:49 -0300

    encrypts user password

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
 import { z } from "zod";
+import { hash } from "bcrypt";
 
 class UsersController {
-  create(request: Request, response: Response) {
+  async create(request: Request, response: Response) {
     //validar o body com o zod - se não passar na validação, vai retornar um erro
     const bodySchema = z.object({
       name: z.string().trim().min(3),
@@ -13,7 +14,10 @@ class UsersController {
     //recuperar o body da requisição
     const { name, email, password } = bodySchema.parse(request.body);
 
-    return response.json({ message: "create user" });
+    //criptografar a senha - 8 é o numero de rounds
+    const hashedPassword = await hash(password, 8);
+
+    return response.json({ message: "create user", hashedPassword });
   }
 }
 
