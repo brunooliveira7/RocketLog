35039198886b0f928ba8ab22941f7813cc07091f
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-05-14 10:17:19 -0300

    user registration in the bank, check if it exists, and do not return the password

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,23 +1,48 @@
 import { Request, Response } from "express";
 import { z } from "zod";
 import { hash } from "bcrypt";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
 
 class UsersController {
   async create(request: Request, response: Response) {
-    //validar o body com o zod - se não passar na validação, vai retornar um erro
+    //valida o body com o zod - se não passar(dados do user), retorna erro
     const bodySchema = z.object({
       name: z.string().trim().min(3),
       email: z.string().email(),
       password: z.string().min(6),
     });
 
-    //recuperar o body da requisição
+    //recuperar o body da requisição do user
     const { name, email, password } = bodySchema.parse(request.body);
+
+    //verificar usuário com mesmo email
+    const userWithSameEmail = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithSameEmail) {
+      throw new AppError("User with same email already exists");
+    }
 
     //criptografar a senha - 8 é o numero de rounds
     const hashedPassword = await hash(password, 8);
 
-    return response.json({ message: "create user", hashedPassword });
+    //criar o usuário
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    //para não retornar a senha
+    const { password: _, ...userWithoutPassword } = user;
+
+    return response.json(userWithoutPassword);
   }
 }
 
