a5e4482d47805289aef1d3098427fe130aedd969
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-05-16 09:43:37 -0300

    validating environment variable with Zod

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,7 +1,9 @@
+import { env } from "../env";
+
 //define o segredo (JWT - com var de ambiente) e o tempo de expiração - token
 export const authConfig = {
   jwt: {
-    secret: process.env.JWT_SECRET,
+    secret: env.JWT_SECRET,
     expiresIn: "1d",
   },
 };
