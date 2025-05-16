dedc19e731784ea0acb74ea61c3d3eaa4a60f610
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-05-16 11:04:51 -0300

    create authentication middleware - express user typing

---
+++ src/middlewares/ensure-authenticated.ts
@@ -0,0 +1,44 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+import { authConfig } from "@/configs/auth";
+import { AppError } from "@/utils/AppError";
+
+interface TokenPayload {
+  role: string;
+  sub: string;
+}
+
+function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  //verifica se o usuário está autenticado por meio de um token JWT
+  try {
+    //obtendo o token do cabeçalho da requisição
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new AppError("JWT token is missing", 401);
+    }
+
+    //separando o token do bearer
+    const [, token] = authHeader.split(" ");
+
+    const { role, sub: user_id } = verify(
+      token,
+      authConfig.jwt.secret
+    ) as TokenPayload; //tipagem do token
+
+    //adicionando o id do usuário e o role no request
+    request.user = {
+      id: user_id,
+      role,
+    };
+
+    return next();
+
+  } catch (error) {
+    throw new AppError("Invalid JWT token", 401);
+  }
+}
