5313c625260ca8e20879b8c2bc81e71d71eb4692
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-05-17 16:11:02 -0300

    create delivery log in controller - movements

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { z } from "zod";
+import { prisma } from "@/database/prisma"
+import { AppError } from "@/utils/AppError";
 
 //rota para criar um novo registro de log de entrega
 class DeliveryLogsController {
@@ -9,7 +11,33 @@ class DeliveryLogsController {
       description: z.string(),
     });
 
-    return response.json({
+    const { delivery_id, description } = bodySchema.parse(request.body);
+
+    //procura se existe uma entrega com o id informado
+    const delivery = await prisma.delivery.findUnique({
+      where: {
+        id: delivery_id,
+      },
+    });
+
+    //se não existir a entrega
+    if (!delivery) {
+     throw new AppError("Delivery not found", 404);
+    }
+
+    //entrega em processamento, não foi enviado
+    if (delivery.status === "processing") {
+      throw new AppError("Change status to shipped", 404);
+    }
+
+    await prisma.deliveryLog.create({
+      data: {
+        deliveryId: delivery_id,
+        description,
+      },
+    });
+
+    return response.status(201).json({
       message: "log created",
     });
   }
