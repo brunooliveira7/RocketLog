63312166eee766794f3438771030f09b35d361c3
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-05-19 11:47:02 -0300

    user registration test

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,5 +1,22 @@
+import request from "supertest";
+import { app } from "@/app";
+
+
 describe("Users Controller", () => {
-  it("test", () => {
-    console.log("Users Controller");
+  //tester se o usuário foi criado
+  it("should create a new user", async () => {
+    //fazer uma requisição POST para a rota /users com os dados do usuário para cadastrar
+    const response = await request(app).post("/users").send({
+      name: "Test User",
+      email: "testuser@example.com",
+      password: "password123",
+    });
+
+    //verificar se a resposta tem o status 201 (created) - igual userController.ts
+    expect(response.status).toBe(201);
+    //verificar se o corpo da resposta contém as propriedades esperadas
+    expect(response.body).toHaveProperty("id");
+    //verificar se o nome do usuário foi criado corretamente
+    expect(response.body.name).toBe("Test User");
   });
 });
